4d98aa47cfa112a0e2f665255c580a975939c387
rio Lv <68161803+Rio-Lv@users.noreply.github.com>
2025-06-07 14:10:12 +0100

    Upgrade to React 18 and update tooling

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders LWONG CONSULTING title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/LWONG CONSULTING/i);
   expect(linkElement).toBeInTheDocument();
 });
